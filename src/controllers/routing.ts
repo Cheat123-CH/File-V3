511c249239c36ba7ae2748ae7103c471f9c2de13
CHAN Suvannet <chansuvannet999@gmail.com>
2024-10-22 00:09:29 +0700

    add image and update route

--- src/controllers/routing.ts
+++ src/controllers/routing.ts
@@ -6,7 +6,7 @@ import { UploadValidation } from './validation';
 const fileRouter = express.Router();
 
 fileRouter.get("/:filename",        FileController.read);
-fileRouter.post("/upload",          singleFileMulter, FileController.upload);
+fileRouter.post("/upload-single",   singleFileMulter, FileController.upload);
 fileRouter.post("/upload-base64",   UploadValidation, FileController.base64);
 
 export default fileRouter;
