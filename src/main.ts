7b8b92040e102bd49cb9af4a27dd195da13d8b0f
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-04-28 23:49:53 +0700

    add start script

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'view'));
 // Init application
 app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
-app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(cors());
 
