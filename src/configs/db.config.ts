a41c4e95c6c544d60351a80d1af66e8f738659ac
Cheat123-CH <sokhcheatmet2024@gmail.com>
2026-05-01 09:32:27 +0700

    fix render db

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 class DatabaseConfig {
     private static commonConfig = {
-        host: process.env.DB_HOST || 'dpg-d7o8gr9f9bms738v575g-a.oregon-postgres.render.com',
-        port: Number(process.env.DB_PORT) || 5432,
-        username: process.env.DB_USERNAME || 'auto',
-        password: process.env.DB_PASSWORD || 'QPs1cJpxxKDvSmOssKfcKL1E7eKXtwEA',
-        database: process.env.DB_DATABASE || 'pos_v2_uyb0',
+        host:     process.env.DB_HOST,
+        port:     Number(process.env.DB_PORT) || 5432,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         models: [__dirname + '/../models/**/*.model.{ts,js}'],
         logging: false,
         dialectOptions: {
